1cee585188002a936e4ea9f6717593ad68b01fbc
arthu <arthur03moulin@gmail.com>
2022-11-08 11:53:44 +0100

    initialisation

---
+++ server-raw.js
@@ -0,0 +1,22 @@
+import http from 'http'
+
+const server = http.createServer((request, response) => {
+    if(request.url === 'api/products' && request.method === 'GET'){
+        response.writeHead(200, {'Content-type' : 'application/json'})
+        response.end(JSON.stringify({
+            id: 1,
+            name: "phone",
+            color: "black"
+        }))
+    }else {
+        response.writeHead(404, {'Content-type' : 'application/json'})
+        response.end(JSON.stringify({
+            message: "Route not found, please use api/products endpoint"
+        }))
+        
+    }
+})
+
+const PORT = process.env.PORT || 500
+
+server.listen (PORT, ()=> console.log('server running on port ${PORT}'))
